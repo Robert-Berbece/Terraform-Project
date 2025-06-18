b8d7b1652ae3b15a3116608221fe968206835208
Robert-Berbece <robertberbece911@gmail.com>
2025-06-18 14:12:50 +0300

    Added s3 backend configuration for terraform state

--- main.tf
+++ main.tf
@@ -77,3 +77,13 @@ data "aws_ami" "amazon_linux" {
     values = ["hvm"]
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "s3-robertp-terraform-state-bucket"
+    key            = "episode2/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
